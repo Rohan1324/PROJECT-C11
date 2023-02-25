a860a5502f8f762c9042afda17b42a59deb872cf
Rohan1324 <99813137+Rohan1324@users.noreply.github.com>
2023-02-25 19:30:29 +0530

    Add files via upload

---
+++ Move_file.py
@@ -0,0 +1,8 @@
+import os
+import shutil
+
+
+path = "Desktop/whitehat jr/C111 Image/feather.jifif"
+root,extension = os.path.splitext(path)
+print("the root element are: ",root)
+print("the extension element are: ",extension)
